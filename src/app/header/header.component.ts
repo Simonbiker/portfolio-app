166f390545ed8b08b4c578ce3ed0b59fee9589f2
Simonbiker <smacr25@gmail.com>
2020-12-16 13:01:57 +0000

    Adding new component and correct index file

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+  public isShown: boolean = false;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  // tslint:disable-next-line:typedef
+  public userMenuToggle() {
+    this.isShown = !this.isShown;
+  }
+
+}
