7c54d0b85c201339509ea39564e448e80c7f2d9a
Simonbiker <smacr25@gmail.com>
2020-12-17 11:24:54 +0000

    Creating new components. Changing the color of the navbar. Removing unwanted code.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContactFormComponent } from './contact-form/contact-form.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { SkillTableComponent } from './skill-table/skill-table.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent,
+    ContactFormComponent,
+    ProjectsComponent,
+    SkillTableComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/skill-table/skill-table.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-skill-table',
+  templateUrl: './skill-table.component.html',
+  styleUrls: ['./skill-table.component.scss']
+})
+export class SkillTableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
